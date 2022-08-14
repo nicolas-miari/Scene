1a377b0b483f28a6e3cde9ba255165935f99eda4
Nicolás Miari <nicolasmiari@gmail.com>
2022-08-14 14:51:29 +0900

    Update Entity dependency to version 1.0.0 (custom codable).

--- Sources/Scene/Scene.swift
+++ Sources/Scene/Scene.swift
@@ -5,7 +5,7 @@ import Entity
 
 public final class Scene: Codable {
 
-  private var entityTable: [EntityIdentifier: ComponentSet]
+  private var entityTable: [EntityIdentifier: ComponentList]
 
   public init() {
     entityTable = [:]
